77845855bde5d1d4d63517739cdc8ec5a5291340
Aldeesh <fastbyte@Aldeeshs-MacBook-Pro.local>
2023-10-08 13:30:11 +0530

    Updated idk what tho

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use std::env::{self};
-use log::{info};
+use colored::*;
+use std::{env::{self}, fs::OpenOptions, io::Read};
+use log::info;
 
 #[derive(Debug)]
 pub struct ToDo {
@@ -8,9 +9,61 @@ pub struct ToDo {
 }
 
 impl ToDo {
-    pub fn new() -> ToDo {
-        let file_path: String = env::var("TODO_PATH").expect("Expected TODO_PATH to be set as env variable.");
-        info!("Creating a new ToDo file at path: {} with empty list of todos", file_path);
-        ToDo { todo: vec![], file_path }
+    pub fn get_or_create() -> ToDo {
+        // 1. Check env var
+        let file_path: String = match env::var("TODO_PATH") {
+            Ok(value) => value,
+            Err(e) => {
+                info!("Unable to find TODO_PATH env variable. Error: {}", e);
+                let home_path = match env::var("HOME") {
+                    Ok(p) => p,
+                    Err(_) => "/tmp".to_string(),
+                };
+                let path = format!("{}/.todo", home_path);
+                env::set_var("TODO_PATH", &path);
+                path
+            },
+        };
+
+        // 2. Check file and read to todo vec
+        let mut file_contents = String::new();
+
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&file_path)
+            .expect(&format!("Unable to create/load file: {}", file_path));
+
+        file.read_to_string(&mut file_contents).unwrap();
+
+        info!("Contents of file {} is: {}", file_path, file_contents);
+
+        // load file_content into vec
+        let todo: Vec<String> = file_contents.lines().map(str::to_string).collect();
+
+        ToDo { todo, file_path }
+    }
+
+    pub fn list(&self) {
+        for (idx, task) in self.todo.iter().enumerate() {
+            // split "[  ] "(not done) or "[x] "(done)
+            // append status at end
+            let mut data = String::new();
+            let status = &task[..4];
+            let content = &task[4..];
+            if status == "[ ] " {
+                data = format!("{}. {}", idx, content.bold());
+            } else if status == "[x] " {
+                data = format!("{}. {}", idx, content.strikethrough());
+            }
+            println!("{}", data);
+        }
+    }
+
+    pub fn add(&self, tasks: &[String]) {
+        for task in  tasks {
+            print!("{} ", task);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+use std::env;
+
 use todo_cli::ToDo;
 
 fn main() {
-    let todo: ToDo = ToDo::new();
-    println!("{:?}", todo);
+    let todo: ToDo = ToDo::get_or_create();
+    // println!("{:?}", todo);
+    
+    // cli args:
+    // 1. list -> list all todos
+    // 2. add <string> -> add a todo
+    // 3. done <index of todo> -> mark a todo as done
+    // 4. rem <index of todo> -> remove a todo (irrespective of status)
+    // 5. 
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let cmd = args.get(1).unwrap();
+        match &cmd[..] {
+            "list" => todo.list(),
+            "add" => todo.add(&args[2..]),
+            _ => panic!("unexpected...."),
+        };
+    }
 }
